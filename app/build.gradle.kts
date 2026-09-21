d55cc35c9f39145a1e50567928d0c6579c2b5eb8
Yohei Oba <gomatty@gmail.com>
2026-09-21 19:10:02 +0900

    20260921-D01:README修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ fun versionCodeFrom(name: String): Int {
     return date * 100 + seq
 }
 
-val appVersionName = "20260920-D01"
+val appVersionName = "20260921-D01"
 android {
     namespace = "io.github.eightbrows.gpslogger"
     compileSdk {
